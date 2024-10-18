ac080c75f0157db628c3055cb781fbebbf710dee
UwU-Stalin-UwU <JoBidon@tutamail.com>
2024-10-18 15:04:11 +0300

    +3 задача

--- sum of series.py
+++ sum of series.py
@@ -1,9 +1,32 @@
-l=input()
-e=input()
-s=input()
-smy=l
-n=1
-while smy<=s :
-    smy=smy+1/(n**2)
-    n+=1
-print(round(smy, e))
+from decimal import Decimal as dec
+
+input_data = open('input.txt', 'r')
+data = input_data.read()
+data = data.split()
+
+l = dec(data[0])
+e = int(data[1])
+s = dec(data[2])
+kol = 1
+sum = 0             #переменные необходимые для решения
+
+p = s - l #число которое необходимо добавить к l, следовательно искомая сумма
+s = round(s, e)
+
+while sum < p:
+    sum += (1/ kol ** 2)
+    kol += 1                          #вычисление количества итераций
+    if s - l < 1:
+        kol = 1           #проверка разницы на единицу
+    
+else:
+    if sum - float(s) < float(s) - (sum - (1/ (kol -1) ** 2)):
+        kol = kol - 1
+    kol -= 1
+
+output_data = open('output.txt', 'w')
+output_data.write(str(kol))
+
+#закрытие файлов 
+input_data.close()
+output_data.close()
